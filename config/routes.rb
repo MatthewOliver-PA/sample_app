8e19cf1f90326500ebe4f8a198f61442d62b6f4a
Matthew Oliver <matt.oliver@paconsulting.com>
2020-02-03 14:54:36 +0000

    added updated css for more modern version of bootstrap. added routes for the users page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   get 'users/new'
   root'static_pages#home'
-  match '/home', to: 'static_pages#home', via: 'get'
-  match '/help', to: 'static_pages#help', via: 'get'
+  match '/home', to: 'static_pages#home',   via: 'get'
+  match '/help', to: 'static_pages#help',   via: 'get'
   match '/about', to: 'static_pages#about', via: 'get'
+  match '/signup', to: 'users#new',         via: 'get'
   # match '/contact', to: 'static_pages#contact', via: 'get'
   # match '/news', to: 'static_pages#news', via: 'get'
-  # match '/sign-in', to: 'static_pages#sign-in', via: 'get'
 end
