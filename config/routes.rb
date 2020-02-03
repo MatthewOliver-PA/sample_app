cd3514a54c03b1d350b8c0721f8fd6d2b661cef7
Matthew Oliver <matt.oliver@paconsulting.com>
2020-02-03 13:36:34 +0000

    added routes for home, help and about

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root'static_pages#home'
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
+  match '/home', to: 'static_pages#home', via: 'get'
+  match '/help', to: 'static_pages#help', via: 'get'
+  match '/about', to: 'static_pages#about', via: 'get'
+
 end
