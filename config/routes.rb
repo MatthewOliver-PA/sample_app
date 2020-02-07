e3e0562d593865e9e1fb0c40a224a4b1f0c5afcb
Matthew Oliver <matt.oliver@paconsulting.com>
2020-02-07 10:56:09 +0000

    signup functionality working, no tests as there is a bug in sqlite that breaks the build

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   match '/help', to: 'static_pages#help',   via: 'get'
   match '/about', to: 'static_pages#about', via: 'get'
   match '/signup', to: 'users#new',         via: 'get'
+  post '/signup',  to: 'users#create'
   resources :users
 end
