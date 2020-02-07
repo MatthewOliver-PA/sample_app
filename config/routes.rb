ae93548e379e2a4e440e0ed5aedb4126393bac5d
Matthew Oliver <matt.oliver@paconsulting.com>
2020-02-07 14:38:19 +0000

    added login page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   get 'users/new'
   root'static_pages#home'
   match '/home', to: 'static_pages#home',   via: 'get'
@@ -6,5 +7,8 @@ Rails.application.routes.draw do
   match '/about', to: 'static_pages#about', via: 'get'
   match '/signup', to: 'users#new',         via: 'get'
   post '/signup',  to: 'users#create'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   resources :users
 end
